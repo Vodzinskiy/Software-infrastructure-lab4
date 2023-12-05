48a55ac09adbff69e12564f9fc7fb035f7a6d9ae
Vodzinskiy <roma.vodzinskiy@gmail.com>
2023-12-05 16:15:24 +0200

    init backend

---
+++ backend/src/test/java/vms/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vms.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
